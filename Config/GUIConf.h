7ebb18a74166d322c31bfd33a9165877d5d2aa34
Cheng Xiaohu <mostiger@163.com>
2016-12-18 23:03:58 +0800

    在工程中加入状态栏c文件

--- Config/GUIConf.h
+++ Config/GUIConf.h
@@ -39,7 +39,7 @@ Purpose     : Configuration of available features and default values
 *
 *       Configuration of available packages
 */
-#define GUI_SUPPORT_TOUCH   (1)  // Support a touch screen (req. win-manager)
+#define GUI_SUPPORT_TOUCH   (0)  // Support a touch screen (req. win-manager)
 #define GUI_SUPPORT_MOUSE   (0)  // Support a mouse
 #define GUI_WINSUPPORT      (1)  // Window manager package available
 #define GUI_SUPPORT_MEMDEV  (1)  // Memory devices available
